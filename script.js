c20a04efd204db617ffda7b58c5a6f3c3ea9c3d4
Valeropss <davidvalerpop@gmail.com>
2025-07-22 00:07:04 -0400

    Update script.js
    Edit 2

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+if (window.innerWidth <= 768) {
+  const myDiv = document.createElement('div');
+  myDiv.textContent = 'This is mobile only!';
+  myDiv.className = 'custom-mobile-div';
+  document.body.appendChild(myDiv);
+}
+
 mapboxgl.accessToken = 'pk.eyJ1IjoidmFsZXJvcHMiLCJhIjoiY21kMXM0dTRlMDN2dzJ2cTMxYmp3enpxZSJ9.RU1rPEOUyH7RXE3Fn337ew';
 
 navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
@@ -16,59 +23,105 @@ var roads = [];
 
 var roadMarkers = [];
 
+var roadMarkerObj = [];
+
+const road1 = {
+    name: "Fenmar Drive",
+    roadCoords: "point(-79.55579 43.76242)",
+    markerCoords: [[-79.55579, 43.76242]],
+    light: 160,
+    conditions: 100,
+    signs: 40,
+    intersections: 40,
+    danger: 0,
+}
+
+const road2 = {
+    name: "Steeles Ave W",
+    roadCoords: "point(-79.56367 43.76674)",
+    markerCoords: [[-79.56367, 43.76674]],
+    light: 40,
+    conditions: 40,
+    signs: 100,
+    intersections: 100,
+    danger: 0,
+}
+
+const road3 = {
+    name: "Toryork Dr",
+    roadCoords: "point(-79.56420 43.76259)",
+    markerCoords: [[-79.56420, 43.76259]],
+    light: 60,
+    conditions: 100,
+    signs: 10,
+    intersections: 20,
+    danger: 0,
+}
+
+const road4 = {
+    name: "407 Express",
+    roadCoords: "point(-79.56275 43.77689)",
+    markerCoords: [[-79.56275, 43.77689]],
+    light: 1000,
+    conditions: 1000,
+    signs: 1000,
+    intersections: 1000,
+    danger: 0,
+}
+
+const road5 = {
+    name: "Fenmar Drive 2",
+    roadCoords: "point(-79.54069 43.75807)",
+    markerCoords: [[-79.54069, 43.75807]],
+    light: 40,
+    conditions: 70,
+    signs: 40,
+    intersections: 40,
+    danger: 0,
+}
+
+const road6 = {
+    name: "Fenmar Drive 3",
+    roadCoords: "point(-79.56439 43.76615)",
+    markerCoords: [[-79.56439, 43.76615]],
+    light: 160,
+    conditions: 100,
+    signs: 40,
+    intersections: 40,
+    danger: 0,
+}
+
+const road7 = {
+    name: "Weston Road 1",
+    roadCoords: "point(-79.54549 43.76551)",
+    markerCoords: [[-79.54549, 43.76551]],
+    light: 80,
+    conditions: 0,
+    signs: 0,
+    intersections: 80,
+    danger: 0,
+}
+
+const road8 = {
+    name: "Ormont Dr 1",
+    roadCoords: "point(-79.54041 43.76588)",
+    markerCoords: [[-79.54041, 43.76588]],
+    light: 0,
+    conditions: 160,
+    signs: 0,
+    intersections: 0,
+    danger: 0,
+}
+
+
+
+
+
+
+
+roads.push(road1, road2, road3, road4, road5, road6, road7, road8);
+
 function setupMap(center, excluded) {
-
-    
-
-    const road1 = {
-        name: "Fenmar Drive",
-        roadCoords: "point(-79.55579 43.76242),point(-79.56439 43.76615)",
-        markerCoords: [[-79.55579, 43.76242], [-79.56439, 43.76615]],
-        light: 160,
-        conditions: 100,
-        signs: 40,
-        intersections: 40,
-        danger: 0,
-    }
-
-    const road2 = {
-        name: "Steeles Ave W",
-        roadCoords: "point(-79.56367 43.76674)",
-        markerCoords: [[-79.56367, 43.76674]],
-        light: 40,
-        conditions: 40,
-        signs: 100,
-        intersections: 100,
-        danger: 0,
-    }
-
-    const road3 = {
-        name: "Toryork Dr",
-        roadCoords: "point(-79.56420 43.76259)",
-        markerCoords: [[-79.56420, 43.76259]],
-        light: 60,
-        conditions: 100,
-        signs: 10,
-        intersections: 20,
-        danger: 0,
-    }
-
-    const road4 = {
-        name: "407 Express",
-        roadCoords: "point(-79.56275 43.77689)",
-        markerCoords: [[-79.56275, 43.77689]],
-        light: 1000,
-        conditions: 1000,
-        signs: 1000,
-        intersections: 1000,
-        danger: 0,
-    }
-
-    roads.push(road1);
-    roads.push(road2);
-    roads.push(road3);
-    roads.push(road4);
-
 
     const map = new mapboxgl.Map({
         container: 'map',
@@ -78,27 +131,25 @@ function setupMap(center, excluded) {
         hash: true
     });
 
-    //const searchBox = new MapboxSearchBox;
-    //searchBox.accessToken = 'pk.eyJ1IjoidmFsZXJvcHMiLCJhIjoiY21kMXM0dTRlMDN2dzJ2cTMxYmp3enpxZSJ9.RU1rPEOUyH7RXE3Fn337ew';
-    //map.addControl(searchBox);
-
     for (var marker of roadMarkers) {
-        const avoidMarker = document.createElement('div');
+        var avoidMarker = document.createElement('div');
             avoidMarker.style.backgroundColor = 'red';
-            avoidMarker.style.width = '15px';
-            avoidMarker.style.height = '15px';
+            avoidMarker.style.width = '18px';
+            avoidMarker.style.height = '18px';
             avoidMarker.style.borderRadius = '50%';
             avoidMarker.style.display = 'flex';
             avoidMarker.style.alignItems = 'center';
             avoidMarker.style.justifyContent = 'center';
             avoidMarker.style.color = 'white';
-            avoidMarker.style.fontSize = '10px';
+            avoidMarker.style.fontSize = '15px';
             avoidMarker.style.fontWeight = 'bold';
-            avoidMarker.innerText = '✖️'; // or 🚫 or ⚠️
+            avoidMarker.innerText = '🚫'; // or ✖️ or ⚠️
 
-        new mapboxgl.Marker(avoidMarker)
+        var newMarker = new mapboxgl.Marker(avoidMarker)
             .setLngLat(marker)
             .addTo(map);
+        
+        roadMarkerObj.push(newMarker);
     }
     
 
@@ -124,17 +175,22 @@ function setupMap(center, excluded) {
         var signage = document.getElementById("signage").value;
         var trafficLights = document.getElementById("trafficLights").value;
         
+
+        map.remove();
+        roadMarkers = [];
+        roadMarkerObj = [];
+
         var excludedRoads = "";
         var roadNamesToPrint = "";
         for (var road of roads) {
-            road.danger = lightlvl * road.light + roadCond * road.conditions + signage * road.signs + trafficLights * road.intersections
-            if (road.danger >= 1500) {
+            var danger = lightlvl * road.light + roadCond * road.conditions + signage * road.signs + trafficLights * road.intersections
+            if (danger >= 1500) {
+                for (var marker of road.markerCoords) {
+                    roadMarkers.push(marker);
+                }
                 if (excludedRoads == "") {
                     excludedRoads = excludedRoads + road.roadCoords;
                     roadNamesToPrint = roadNamesToPrint + road.name;
-                    for (var marker of road.markerCoords) {
-                        roadMarkers.push(marker);
-                    }
                 } else {
                     excludedRoads = excludedRoads + "," + road.roadCoords;
                     roadNamesToPrint = roadNamesToPrint + ", " + road.name
@@ -143,8 +199,12 @@ function setupMap(center, excluded) {
             }
             
         }
+        
+
         setupMap(center, excludedRoads);
         alert("Roads excluded: " + roadNamesToPrint);
+
+        
     }
 
 
